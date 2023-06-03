5389fdacd4e263ebb338fb11c87b07a393885e14
hiyouga <hiyouga@buaa.edu.cn>
2023-06-03 18:19:01 +0800

    use low_cpu_mem_usage to speed up loading
    Former-commit-id: 7891e4c200566a4a47088e93efd1fbebcb46528e

--- src/export_model.py
+++ src/export_model.py
@@ -13,7 +13,7 @@ def main():
     model_args, training_args, finetuning_args = parser.parse_args_into_dataclasses()
 
     model, tokenizer = load_pretrained(model_args, finetuning_args)
-    model.save_pretrained(training_args.output_dir, max_shard_size="1GB")
+    model.save_pretrained(training_args.output_dir, max_shard_size="10GB")
     tokenizer.save_pretrained(training_args.output_dir)
 
     print("model and tokenizer have been saved at:", training_args.output_dir)
