5f44112cf5cfa04dceadf944201c5ab9b5784ade
hiyouga <hiyouga@buaa.edu.cn>
2023-06-04 00:08:56 +0800

    support QLoRA
    Former-commit-id: d89597e28fe9b91246e58c55eeb9082436940481

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,6 @@
 import os
 import json
+import torch
 from typing import List, Literal, Optional
 from dataclasses import asdict, dataclass, field
 
@@ -206,6 +207,10 @@ class FinetuningArguments:
         metadata={"help": "Name(s) of target modules to apply LoRA. Use comma to separate multiple modules. \
                   LLaMA choices: [\"q_proj\", \"k_proj\", \"v_proj\", \"o_proj\", \"up_proj\", \"down_proj\"], \
                   BLOOM choices: [\"query_key_value\", \"dense\", \"dense_\"]"}
+    )
+    compute_dtype: Optional[torch.dtype] = field(
+        default=None,
+        metadata={"help": "Used in quantization configs. Do not specify this argument manually."}
     )
 
     def __post_init__(self):
