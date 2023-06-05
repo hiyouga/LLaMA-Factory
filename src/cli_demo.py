3d8d5ee5d54102dd73856fac3a80922ea3104a06
hiyouga <hiyouga@buaa.edu.cn>
2023-06-05 21:32:18 +0800

    add API demo from #1

--- src/cli_demo.py
+++ src/cli_demo.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 # Implements stream chat in command line for fine-tuned models.
-# Usage: python cli_demo.py --checkpoint_dir path_to_checkpoint
+# Usage: python cli_demo.py --model_name_or_path path_to_model --checkpoint_dir path_to_checkpoint
 
 
 from utils import (
