8849bee7636fcb5eaacdce3cbff0fe2ccc8681cd
hiyouga <hiyouga@buaa.edu.cn>
2023-06-06 21:36:37 +0800

    recover logging
    Former-commit-id: 13d1f0709c774bb5ec5fb2b4e3c66b2f1226afd2

--- src/utils/ppo.py
+++ src/utils/ppo.py
@@ -16,12 +16,12 @@ from .config import FinetuningArguments
 
 from .other import (
     AverageMeter,
-    get_main_logger,
+    get_logger,
     get_logits_processor
 )
 
 
-logger = get_main_logger(__name__)
+logger = get_logger(__name__)
 
 
 def replace_model(model: AutoModelForCausalLMWithValueHead, target: Literal["default", "reward"]) -> None:
