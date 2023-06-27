9aeb9ecd10e3107e21443430c8e718875a33f734
hiyouga <hiyouga@buaa.edu.cn>
2023-06-27 21:40:11 +0800

    support save full model, replace BOS token

--- src/utils/peft_trainer.py
+++ src/utils/peft_trainer.py
@@ -16,8 +16,6 @@ from transformers import (
 from transformers.trainer import TRAINING_ARGS_NAME
 from transformers.modeling_utils import unwrap_model
 
-from peft.utils.other import WEIGHTS_NAME
-
 from .config import FinetuningArguments
 
 from .other import (
@@ -98,18 +96,22 @@ class PeftTrainer(Seq2SeqTrainer):
         logger.info(f"Saving model checkpoint to {output_dir}")
         model = unwrap_model(self.model)
 
-        if hasattr(model, "pretrained_model"): # for models with valuehead
+        if hasattr(model, "pretrained_model"): # for models with valuehead (currently using LoRA only)
             backbone_model = getattr(model, "pretrained_model")
+            torch.save(get_state_dict(getattr(model, "v_head")), os.path.join(output_dir, VALUE_HEAD_FILE_NAME))
         else:
             backbone_model = model
 
-        if hasattr(backbone_model, "peft_config"): # peft methods
-            backbone_model.save_pretrained(output_dir, state_dict=get_state_dict(backbone_model)) # save lora weights
-        else:
-            torch.save(get_state_dict(backbone_model), os.path.join(output_dir, WEIGHTS_NAME)) # save trainable weights
-
-        if hasattr(model, "v_head"): # save valuehead weights
-            torch.save(get_state_dict(getattr(model, "v_head")), os.path.join(output_dir, VALUE_HEAD_FILE_NAME))
+        if self.finetuning_args.finetuning_type == "lora":
+            backbone_model.save_pretrained(output_dir, state_dict=get_state_dict(backbone_model))
+        else: # freeze/full tuning
+            backbone_model.save_pretrained(
+                output_dir,
+                state_dict=get_state_dict(backbone_model),
+                safe_serialization=self.args.save_safetensors
+            )
+            if self.tokenizer is not None:
+                self.tokenizer.save_pretrained(output_dir)
 
         with open(os.path.join(output_dir, TRAINING_ARGS_NAME), "w", encoding="utf-8") as f:
             f.write(self.args.to_json_string() + "\n")
@@ -122,11 +124,14 @@ class PeftTrainer(Seq2SeqTrainer):
         Subclass and override to inject custom behavior. It should not be directly used by external scripts.
         """
         logger.info(f"Loading best model from {self.state.best_model_checkpoint} (score: {self.state.best_metric}).")
-        model = unwrap_model(self.model)
-        if hasattr(model, "peft_config"): # peft methods
-            model.load_adapter(self.state.best_model_checkpoint, getattr(model, "active_adapter"))
-        else:
-            load_trainable_params(model, self.state.best_model_checkpoint)
 
-        if hasattr(model, "v_head"):
-            load_valuehead_params(model, self.state.best_model_checkpoint)
+        model = unwrap_model(self.model)
+        if self.finetuning_args.finetuning_type == "lora":
+            model.load_adapter(self.state.best_model_checkpoint, getattr(model, "active_adapter"))
+            if hasattr(model, "v_head") and load_valuehead_params(model, self.state.best_model_checkpoint):
+                model.v_head.load_state_dict({
+                    "summary.weight": getattr(model, "reward_head_weight"),
+                    "summary.bias": getattr(model, "reward_head_bias")
+                })
+        else: # freeze/full-tuning
+            load_trainable_params(model, self.state.best_model_checkpoint)
