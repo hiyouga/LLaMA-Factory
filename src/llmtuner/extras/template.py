18c5eb4a44534a21e02b1581c5ac5b22ffdaeb98
hiyouga <hiyouga@buaa.edu.cn>
2023-07-19 00:44:49 +0800

    add LLaMA2 template
    Former-commit-id: 38eb1aaf550b6b5952d757c37eb274587c59dece

--- src/llmtuner/extras/template.py
+++ src/llmtuner/extras/template.py
@@ -96,6 +96,27 @@ register_template(
 
 
 r"""
+Supports: https://huggingface.co/meta-llama/Llama-2-7b-chat-hf
+          https://huggingface.co/meta-llama/Llama-2-13b-chat-hf
+          https://huggingface.co/meta-llama/Llama-2-70b-chat-hf
+"""
+register_template(
+    name="llama2",
+    prefix="<<SYS>>\nYou are a helpful, respectful and honest assistant. "
+           "Always answer as helpfully as possible, while being safe.  "
+           "Your answers should not include any harmful, unethical, "
+           "racist, sexist, toxic, dangerous, or illegal content. "
+           "Please ensure that your responses are socially unbiased and positive in nature.\n"
+           "If a question does not make any sense, or is not factually coherent, "
+           "explain why instead of answering something not correct. "
+           "If you don't know the answer to a question, please don't share false information.\n<</SYS>>\n\n",
+    prompt=" [INST] {query} [/INST] ",
+    sep="",
+    use_history=True
+)
+
+
+r"""
 Supports: https://huggingface.co/tatsu-lab/alpaca-7b-wdiff
           https://github.com/ymcui/Chinese-LLaMA-Alpaca
 """
