23e38950330d911e119a84501781679c7d5845d0
hiyouga <hiyouga@buaa.edu.cn>
2023-07-20 15:08:57 +0800

    simplify code
    Former-commit-id: d3731754ab7c28ae81f60784e0e4213f279d93fe

--- src/llmtuner/__init__.py
+++ src/llmtuner/__init__.py
@@ -1,7 +1,4 @@
-from llmtuner.api import create_app
 from llmtuner.chat import ChatModel
-from llmtuner.tuner import get_train_args, get_infer_args, load_model_and_tokenizer, run_pt, run_sft, run_rm, run_ppo
-from llmtuner.webui import create_ui
 
 
 __version__ = "0.1.1"
