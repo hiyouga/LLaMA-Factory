a7a12077fc7cbb89a1d4dd5065583c83b07c2f2c
YC Chen <ycc.tw.email@gmail.com>
2023-07-27 11:42:04 +0800

    [feature] Add template

--- src/llmtuner/extras/template.py
+++ src/llmtuner/extras/template.py
@@ -241,3 +241,13 @@ register_template(
     sep="<|end|>\n",
     use_history=True
 )
+
+
+register_template(
+    name="mr_chat",
+    prefix="A chat between a curious human (<|HUMAN|>) and an artificial intelligence assistant (<|ASSISTANT|>). "
+           "The assistant gives helpful, detailed, and polite answers to the human's questions.",
+    prompt="<|HUMAN|>: {query}\n\n<|ASSISTANT|>: ",
+    sep="\n",
+    use_history=True
+)
