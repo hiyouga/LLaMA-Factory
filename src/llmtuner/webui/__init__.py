1538eccc09a9979e464b359596417d25ec4e2775
hiyouga <hiyouga@buaa.edu.cn>
2023-08-03 13:28:28 +0800

    update web demo
    Former-commit-id: 5b6ad9adb665096bfb36dc90789a1d4a16345122

--- src/llmtuner/webui/__init__.py
+++ src/llmtuner/webui/__init__.py
@@ -1,4 +1,1 @@
-from llmtuner.webui.chat import WebChatModel
-from llmtuner.webui.interface import create_ui
-from llmtuner.webui.manager import Manager
-from llmtuner.webui.components import create_chat_box
+from llmtuner.webui.interface import create_ui, create_web_demo
