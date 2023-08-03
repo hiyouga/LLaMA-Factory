471ef692188e2ab576a27fa9b6c27e883c2ca603
hiyouga <hiyouga@buaa.edu.cn>
2023-08-03 13:28:28 +0800

    update web demo
    Former-commit-id: 53d6dc396dbfb1d25236bbc3075404cd5be6b376

--- src/api_demo.py
+++ src/api_demo.py
@@ -1,8 +1,3 @@
-# coding=utf-8
-# Implements API for fine-tuned models in OpenAI's format. (https://platform.openai.com/docs/api-reference/chat)
-# Usage: python api_demo.py --model_name_or_path path_to_model --checkpoint_dir path_to_checkpoint
-# Visit http://localhost:8000/docs for document.
-
 import uvicorn
 
 from llmtuner import ChatModel, create_app
@@ -12,6 +7,7 @@ def main():
     chat_model = ChatModel()
     app = create_app(chat_model)
     uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    # Visit http://localhost:8000/docs for document.
 
 
 if __name__ == "__main__":
