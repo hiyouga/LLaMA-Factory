4b841a6b35585120c65e2718d6002c69cc40b925
hiyouga <hiyouga@buaa.edu.cn>
2023-08-08 17:55:55 +0800

    fix bug

--- src/llmtuner/tuner/core/loader.py
+++ src/llmtuner/tuner/core/loader.py
@@ -84,10 +84,7 @@ def load_model_and_tokenizer(
         if model_args.quantization_bit == 8:
             require_version("bitsandbytes>=0.37.0", "To fix: pip install bitsandbytes>=0.37.0")
             config_kwargs["load_in_8bit"] = True
-            config_kwargs["quantization_config"] = BitsAndBytesConfig(
-                load_in_8bit=True,
-                llm_int8_threshold=6.0
-            )
+            config_kwargs["quantization_config"] = BitsAndBytesConfig(load_in_8bit=True)
 
         elif model_args.quantization_bit == 4:
             require_version("bitsandbytes>=0.39.0", "To fix: pip install bitsandbytes>=0.39.0")
