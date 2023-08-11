7bd4c59b7e0941fe37647a2be0d18732db982059
hiyouga <hiyouga@buaa.edu.cn>
2023-08-12 00:25:29 +0800

    fix unusual output of 8bit models #278 #391
    Former-commit-id: dd51c242032ce3f878cb191dc144536db4a2bb45

--- src/llmtuner/tuner/core/loader.py
+++ src/llmtuner/tuner/core/loader.py
@@ -92,7 +92,7 @@ def load_model_and_tokenizer(
             )
 
         is_mergeable = False
-        config_kwargs["device_map"] = {"": int(os.environ.get("LOCAL_RANK", "0"))}
+        config_kwargs["device_map"] = {"": int(os.environ.get("LOCAL_RANK", "0"))} if is_trainable else "auto"
         logger.info("Quantizing model to {} bit.".format(model_args.quantization_bit))
 
     # Load and prepare pretrained models (without valuehead).
