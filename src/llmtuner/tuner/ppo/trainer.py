d9e62711a3349d7c6fd3512fb25c709bdfbb311a
hiyouga <hiyouga@buaa.edu.cn>
2023-08-16 22:39:54 +0800

    fix generation

--- src/llmtuner/tuner/ppo/trainer.py
+++ src/llmtuner/tuner/ppo/trainer.py
@@ -74,6 +74,8 @@ class PPOPeftTrainer(PPOTrainer, PeftTrainer):
 
         # Keyword arguments for `model.generate`
         gen_kwargs = self.generating_args.to_dict()
+        gen_kwargs["eos_token_id"] = self.tokenizer.eos_token_id
+        gen_kwargs["pad_token_id"] = self.tokenizer.pad_token_id
         gen_kwargs["logits_processor"] = get_logits_processor()
         gen_kwargs["stopping_criteria"] = get_stopping_criteria(self.tokenizer.additional_special_tokens_ids)
 
