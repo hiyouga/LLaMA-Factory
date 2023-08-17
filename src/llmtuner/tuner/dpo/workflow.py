58f13e22da18babed0d2d4348474e07745da8fa5
hiyouga <hiyouga@buaa.edu.cn>
2023-08-18 01:41:17 +0800

    update training resuming

--- src/llmtuner/tuner/dpo/workflow.py
+++ src/llmtuner/tuner/dpo/workflow.py
@@ -50,7 +50,7 @@ def run_dpo(
 
     # Training
     if training_args.do_train:
-        train_result = trainer.train()
+        train_result = trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
         trainer.log_metrics("train", train_result.metrics)
         trainer.save_metrics("train", train_result.metrics)
         trainer.save_state()
