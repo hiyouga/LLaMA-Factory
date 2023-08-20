922ca89c1dd7a73c27e16952c86bb2cfb5e976a8
hiyouga <hiyouga@buaa.edu.cn>
2023-08-20 14:07:11 +0800

    fix ppo trainer #551
    Former-commit-id: 050a5447c191b8c50a0826a0f03bae499bff8b48

--- src/llmtuner/tuner/ppo/utils.py
+++ src/llmtuner/tuner/ppo/utils.py
@@ -1,4 +1,7 @@
-from typing import TYPE_CHECKING, Literal
+import torch
+from typing import TYPE_CHECKING, Dict, List, Literal, Optional, Tuple
+
+from llmtuner.extras.constants import LAYERNORM_NAMES
 
 if TYPE_CHECKING:
     from trl import AutoModelForCausalLMWithValueHead
@@ -15,3 +18,23 @@ def replace_model(model: "AutoModelForCausalLMWithValueHead", target: Literal["d
         "summary.weight": getattr(model, "{}_head_weight".format(target)),
         "summary.bias": getattr(model, "{}_head_bias".format(target))
     })
+
+
+def cast_layernorm_dtype(
+    model: "AutoModelForCausalLMWithValueHead",
+    compute_dtype: torch.dtype,
+    layer_norm_params: Optional[Dict[str, torch.Tensor]] = None,
+    layer_norm_names: Optional[List[str]] = LAYERNORM_NAMES
+) -> Tuple["AutoModelForCausalLMWithValueHead", Dict[str, torch.Tensor]]:
+
+    layer_norm_state_dict = {}
+
+    for name, param in model.named_parameters():
+        if param.ndim == 1 and any(layer_norm_name in name for layer_norm_name in layer_norm_names):
+            if layer_norm_params is None:
+                layer_norm_state_dict[name] = param.data.detach().clone() # store float32 weights for stability
+                param.data = param.data.to(compute_dtype)
+            else:
+                param.data = layer_norm_params[name] # restore float32 weights
+
+    return model, layer_norm_state_dict
