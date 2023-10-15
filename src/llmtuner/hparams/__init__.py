c2e84d455842be3e9cf3e662bbdfae3ba1b34a35
hiyouga <hiyouga@buaa.edu.cn>
2023-10-15 16:01:48 +0800

    refactor export, fix #1190
    Former-commit-id: 30e60e37023a7c4a2db033ffec0542efa3d5cdfb

--- src/llmtuner/hparams/__init__.py
+++ src/llmtuner/hparams/__init__.py
@@ -1,5 +1,4 @@
 from .data_args import DataArguments
 from .finetuning_args import FinetuningArguments
-from .general_args import GeneralArguments
 from .generating_args import GeneratingArguments
 from .model_args import ModelArguments
