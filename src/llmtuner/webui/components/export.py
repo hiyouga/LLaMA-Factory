89c1a80920ae787ffc311816dc4a6c6e57a92824
hiyouga <hiyouga@buaa.edu.cn>
2023-11-02 21:21:45 +0800

    support pagination in webui preview
    Former-commit-id: c1edb0cf1b2a4d52506fc9e15353dfbe513e5d8f

--- src/llmtuner/webui/components/export.py
+++ src/llmtuner/webui/components/export.py
@@ -1,16 +1,54 @@
 import gradio as gr
-from typing import TYPE_CHECKING, Dict
+from typing import TYPE_CHECKING, Dict, Generator, List
 
-from llmtuner.webui.utils import save_model
+from llmtuner.tuner import export_model
+from llmtuner.webui.common import get_save_dir
+from llmtuner.webui.locales import ALERTS
 
 if TYPE_CHECKING:
     from gradio.components import Component
     from llmtuner.webui.engine import Engine
 
 
-def create_export_tab(engine: "Engine") -> Dict[str, "Component"]:
-    elem_dict = dict()
+def save_model(
+    lang: str,
+    model_name: str,
+    model_path: str,
+    checkpoints: List[str],
+    finetuning_type: str,
+    template: str,
+    max_shard_size: int,
+    export_dir: str
+) -> Generator[str, None, None]:
+    error = ""
+    if not model_name:
+        error = ALERTS["err_no_model"][lang]
+    elif not model_path:
+        error = ALERTS["err_no_path"][lang]
+    elif not checkpoints:
+        error = ALERTS["err_no_checkpoint"][lang]
+    elif not export_dir:
+        error = ALERTS["err_no_export_dir"][lang]
 
+    if error:
+        gr.Warning(error)
+        yield error
+        return
+
+    args = dict(
+        model_name_or_path=model_path,
+        checkpoint_dir=",".join([get_save_dir(model_name, finetuning_type, ckpt) for ckpt in checkpoints]),
+        finetuning_type=finetuning_type,
+        template=template,
+        export_dir=export_dir
+    )
+
+    yield ALERTS["info_exporting"][lang]
+    export_model(args, max_shard_size="{}GB".format(max_shard_size))
+    yield ALERTS["info_exported"][lang]
+
+
+def create_export_tab(engine: "Engine") -> Dict[str, "Component"]:
     with gr.Row():
         export_dir = gr.Textbox()
         max_shard_size = gr.Slider(value=10, minimum=1, maximum=100)
@@ -33,11 +71,9 @@ def create_export_tab(engine: "Engine") -> Dict[str, "Component"]:
         [info_box]
     )
 
-    elem_dict.update(dict(
+    return dict(
         export_dir=export_dir,
         max_shard_size=max_shard_size,
         export_btn=export_btn,
         info_box=info_box
-    ))
-
-    return elem_dict
+    )
