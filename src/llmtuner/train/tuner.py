670ee3934f5b36cb7887e27bf62c3880a1f5dedf
hiyouga <hiyouga@buaa.edu.cn>
2023-11-28 20:52:28 +0800

    fix #1659
    Former-commit-id: e4123129aae59f4123d53c1f5320e3d5e09ae26d

--- src/llmtuner/train/tuner.py
+++ src/llmtuner/train/tuner.py
@@ -38,8 +38,8 @@ def export_model(args: Optional[Dict[str, Any]] = None):
     model_args, _, finetuning_args, _ = get_infer_args(args)
     model, tokenizer = load_model_and_tokenizer(model_args, finetuning_args)
 
-    if getattr(model, "quantization_method", None) == "gptq":
-        raise ValueError("Cannot export a GPTQ quantized model.")
+    if getattr(model, "quantization_method", None) in ["gptq", "awq"]:
+        raise ValueError("Cannot export a GPTQ or AWQ quantized model.")
 
     model.config.use_cache = True
     model.save_pretrained(finetuning_args.export_dir, max_shard_size="{}GB".format(finetuning_args.export_size))
