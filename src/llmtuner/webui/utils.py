79f846bc1f2eb2f40e0f61690e7ec83cc3dde0e7
hiyouga <hiyouga@buaa.edu.cn>
2023-12-01 15:58:50 +0800

    tiny fix

--- src/llmtuner/webui/utils.py
+++ src/llmtuner/webui/utils.py
@@ -44,8 +44,8 @@ def can_quantize(finetuning_type: str) -> Dict[str, Any]:
 def gen_cmd(args: Dict[str, Any]) -> str:
     args.pop("disable_tqdm", None)
     args["plot_loss"] = args.get("do_train", None)
-    cuda_visible_devices = os.environ.get('CUDA_VISIBLE_DEVICES') or "0"
-    cmd_lines = [f"CUDA_VISIBLE_DEVICES={cuda_visible_devices} python src/train_bash.py "]
+    current_devices = os.environ.get("CUDA_VISIBLE_DEVICES", "0")
+    cmd_lines = ["CUDA_VISIBLE_DEVICES={} python src/train_bash.py ".format(current_devices)]
     for k, v in args.items():
         if v is not None and v != "":
             cmd_lines.append("    --{} {} ".format(k, str(v)))
