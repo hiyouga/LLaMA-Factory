c3448a045ccd3afea82821f6667d6e629f3698c3
hiyouga <hiyouga@buaa.edu.cn>
2023-12-11 18:09:40 +0800

    tiny fix
    Former-commit-id: 1f839fc4f278c2a258df22899241fc66a2cca682

--- src/llmtuner/model/utils.py
+++ src/llmtuner/model/utils.py
@@ -189,7 +189,7 @@ def resize_embedding_layer(model: "PreTrainedModel", tokenizer: "PreTrainedToken
     Resize token embeddings.
     """
     old_vocab_size = model.get_input_embeddings().weight.size(0)
-    new_vocab_size = len(tokenizer)
-    if new_vocab_size != old_vocab_size:
-        model.resize_token_embeddings(new_vocab_size, pad_to_multiple_of=64)
-        logger.info("Resized embedding tokens from {} to {}.".format(old_vocab_size, new_vocab_size))
+    if len(tokenizer) != old_vocab_size:
+        model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of=64)
+        new_vocab_size = model.get_input_embeddings().weight.size(0)
+        logger.info("Resized token embeddings from {} to {}.".format(old_vocab_size, new_vocab_size))
