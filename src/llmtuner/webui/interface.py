aa5146a6bc6588e53160ec7ed2a249a7100bdad1
hiyouga <hiyouga@buaa.edu.cn>
2023-12-15 23:50:15 +0800

    fix #1770

--- src/llmtuner/webui/interface.py
+++ src/llmtuner/webui/interface.py
@@ -75,4 +75,4 @@ def create_web_demo() -> gr.Blocks:
 if __name__ == "__main__":
     demo = create_ui()
     demo.queue()
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False, inbrowser=True)
+    demo.launch(server_name="0.0.0.0", share=False, inbrowser=True)
