393e4b0f5a37a66a3806ee2df45dff7b287b843f
hiyouga <hiyouga@buaa.edu.cn>
2023-12-15 23:50:15 +0800

    fix #1770
    Former-commit-id: 8266187cec70bb4bd1b4837d51b09409ec11f93e

--- src/web_demo.py
+++ src/web_demo.py
@@ -4,7 +4,7 @@ from llmtuner import create_web_demo
 def main():
     demo = create_web_demo()
     demo.queue()
-    demo.launch(server_name="0.0.0.0", server_port=7860, share=False, inbrowser=True)
+    demo.launch(server_name="0.0.0.0", share=False, inbrowser=True)
 
 
 if __name__ == "__main__":
