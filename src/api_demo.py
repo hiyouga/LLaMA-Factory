db545efd6df18b68d789cd89b91a701bb993ef4b
hiyouga <hiyouga@buaa.edu.cn>
2024-01-07 17:17:18 +0800

    tiny fix
    Former-commit-id: 0a9986160ccdc9c82efbc83f36d1a3d84859f098

--- src/api_demo.py
+++ src/api_demo.py
@@ -1,3 +1,4 @@
+import os
 import uvicorn
 
 from llmtuner import ChatModel, create_app
@@ -6,8 +7,8 @@ from llmtuner import ChatModel, create_app
 def main():
     chat_model = ChatModel()
     app = create_app(chat_model)
-    print("Visit http://localhost:8000/docs for API document.")
-    uvicorn.run(app, host="0.0.0.0", port=8000, workers=1)
+    print("Visit http://localhost:{}/docs for API document.".format(os.environ.get("API_PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("API_PORT", 8000)), workers=1)
 
 
 if __name__ == "__main__":
