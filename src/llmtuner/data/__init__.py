e40f06cd18a7588b8471874f0775dbf2b5c5df19
hiyouga <hiyouga@buaa.edu.cn>
2024-01-20 20:21:39 +0800

    release v0.5.0
    Former-commit-id: 602bb9b685009b9af234499be278404721542ac7

--- src/llmtuner/data/__init__.py
+++ src/llmtuner/data/__init__.py
@@ -3,4 +3,4 @@ from .template import get_template_and_fix_tokenizer, templates
 from .utils import Role, split_dataset
 
 
-__all__ = ["get_dataset", "get_template_and_fix_tokenizer", "templates", "split_dataset", "Role"]
+__all__ = ["get_dataset", "get_template_and_fix_tokenizer", "templates", "Role", "split_dataset"]
