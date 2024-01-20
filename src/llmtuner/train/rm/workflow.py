2f7684a8ee8636cae92b894225b9474a32cf3a73
hiyouga <hiyouga@buaa.edu.cn>
2024-01-20 19:58:04 +0800

    fix tests
    Former-commit-id: f6d6e00337ebef8740d180836dcb18d0e6a3c59a

--- src/llmtuner/train/rm/workflow.py
+++ src/llmtuner/train/rm/workflow.py
@@ -26,7 +26,7 @@ def run_rm(
     callbacks: Optional[List["TrainerCallback"]] = None
 ):
     model, tokenizer = load_model_and_tokenizer(model_args, finetuning_args, training_args.do_train, add_valuehead=True)
-    dataset = get_dataset(model_args, data_args, tokenizer, training_args, stage="rm")
+    dataset = get_dataset(tokenizer, model_args, data_args, training_args, stage="rm")
     data_collator = PairwiseDataCollatorWithPadding(tokenizer, pad_to_multiple_of=8)
 
     # Update arguments
