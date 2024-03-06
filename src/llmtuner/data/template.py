639e840716e9f07fb5b5fc0ba2768a1b9bde2261
Sanjay Nadhavajhala <sanjay@acorn.io>
2024-03-06 15:58:54 -0800

    add

--- src/llmtuner/data/template.py
+++ src/llmtuner/data/template.py
@@ -808,10 +808,10 @@ _register_template(
         slots=["<|im_start|>assistant\n{{content}}<|im_end|>\n"]
     ),
     format_function=StringFormatter(
-        slots=["<|im_start|>function\n{{content}}<|im_end|>\n"]
+        slots=["<|im_start|><<functions>>\n{{content}}<|im_end|>\n"]
     ),
     format_observation=StringFormatter(
-        slots=["<|im_start|>observation\n{{content}}<|im_end|>\n"]
+        slots=["<|im_start|><<observation>>\n{{content}}<|im_end|>\n"]
     ),
     stop_words=["<|im_end|>", "<|im_start|>"],
     generation_prompt="<|im_start|>assistant\\n",
@@ -1035,7 +1035,9 @@ if __name__ == "__main__":
         messages=messsages, system="you are helpful assistant.", tools=tool_content
     )
     print(type(res))
-    print(f"=======\nEncoded Message: \n {res}\n=======")
+    print(f"=======\nEncoded Message:\n=======")
+    for msg in res:
+        print(msg)
 
     jinja_template = _get_jinja_template(templates["chatml_rubra"], tokenizer)
     print(f"=======\nJinja Template: \n {jinja_template}\n=======")
