cb2bf680c9cf79eb56179d28a491720c4298a434
hiyouga <hiyouga@buaa.edu.cn>
2024-03-07 22:53:45 +0800

    add galore examples
    Former-commit-id: 7230e1177daf4d96a1205565ab9335085cc8f3a7

---
+++ examples/extras/llama_pro/expand.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install -e ../../../.
+
+python ../../../scripts/llama_pro.py \
+    --model_name_or_path meta-llama/Llama-2-7b-hf \
+    --output_dir ../../../models/llama2-7b-pro \
+    --num_expand 8
