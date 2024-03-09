58b6056baeff8eb531bf32e7fd5f6d41cca816a2
hiyouga <hiyouga@buaa.edu.cn>
2024-03-09 21:35:24 +0800

    fix #2766

--- src/llmtuner/chat/chat_model.py
+++ src/llmtuner/chat/chat_model.py
@@ -29,6 +29,7 @@ class ChatModel:
         self._loop = asyncio.new_event_loop()
         self._thread = Thread(target=_start_background_loop, args=(self._loop,), daemon=True)
         self._thread.start()
+        asyncio.run_coroutine_threadsafe(self.engine.start(), self._loop)
 
     def chat(
         self,
