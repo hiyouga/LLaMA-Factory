3271af2afc90f10dcb101aeb9d7e4ef254d2dc0e
hiyouga <hiyouga@buaa.edu.cn>
2024-03-20 20:09:06 +0800

    add orca_dpo_pairs dataset

--- data/belle_multiturn/belle_multiturn.py
+++ data/belle_multiturn/belle_multiturn.py
@@ -2,6 +2,7 @@ import os
 import json
 import datasets
 
+
 _HF_ENDPOINT = os.getenv("HF_ENDPOINT", "https://huggingface.co")
 
 _DESCRIPTION = "BELLE multiturn chat dataset."
@@ -15,9 +16,9 @@ _CITATION = """\
 }
 """
 
-_HOMEPAGE = f"{_HF_ENDPOINT}/datasets/BelleGroup/multiturn_chat_0.8M"
+_HOMEPAGE = "{}/datasets/BelleGroup/multiturn_chat_0.8M".format(_HF_ENDPOINT)
 _LICENSE = "gpl-3.0"
-_URL = f"{_HF_ENDPOINT}/datasets/BelleGroup/multiturn_chat_0.8M/resolve/main/multiturn_chat_0.8M.json"
+_URL = "{}/datasets/BelleGroup/multiturn_chat_0.8M/resolve/main/multiturn_chat_0.8M.json".format(_HF_ENDPOINT)
 
 
 class BelleMultiturn(datasets.GeneratorBasedBuilder):
