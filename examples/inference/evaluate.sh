e341fa59fe828efc5cb78359a273ada7683072be
hiyouga <hiyouga@buaa.edu.cn>
2024-04-02 20:51:21 +0800

    update examples
    Former-commit-id: f22eaeb5bc5329146feb0cc5455fae8ce10380f8

--- examples/inference/evaluate.sh
+++ examples/inference/evaluate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python src/evaluate.py \
+CUDA_VISIBLE_DEVICES=0 python ../../src/evaluate.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --adapter_name_or_path ../../saves/LLaMA2-7B/lora/sft \
     --template vanilla \
