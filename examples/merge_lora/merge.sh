be206df674105087a82381aeb07ef645648bc846
hiyouga <hiyouga@buaa.edu.cn>
2024-04-15 22:14:34 +0800

    update examples
    Former-commit-id: 369294b31c8a03a1cafcee83eb31a817007d3c49

--- examples/merge_lora/merge.sh
+++ examples/merge_lora/merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # DO NOT use quantized model or quantization_bit when merging lora weights
 
-CUDA_VISIBLE_DEVICES= python ../../src/export_model.py \
+CUDA_VISIBLE_DEVICES=0 python ../../src/export_model.py \
     --model_name_or_path meta-llama/Llama-2-7b-hf \
     --adapter_name_or_path ../../saves/LLaMA2-7B/lora/sft \
     --template default \
