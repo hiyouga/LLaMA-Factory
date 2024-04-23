4fd804145924d940afbb1c45bad70c320941850e
BUAADreamer <1428195643@qq.com>
2024-04-23 18:45:43 +0800

    add multimodal LLM BLIP-2 and InstructBLIP

--- src/llmtuner/model/__init__.py
+++ src/llmtuner/model/__init__.py
@@ -1,10 +1,11 @@
-from .loader import load_model, load_tokenizer
+from .loader import load_model, load_tokenizer, load_processor, load_mm_model
 from .utils import find_all_linear_modules, load_valuehead_params
-
 
 __all__ = [
     "load_model",
+    "load_mm_model",
     "load_tokenizer",
+    "load_processor",
     "load_valuehead_params",
     "find_all_linear_modules",
 ]
---
+++ src/llmtuner/train/sftmm/__init__.py
@@ -0,0 +1,3 @@
+from .workflow import run_sft_mm
+
+__all__ = ["run_sft_mm"]
