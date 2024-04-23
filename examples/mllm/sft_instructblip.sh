37705bbfdc37aba40337115b91d7fac631d6aca7
BUAADreamer <1428195643@qq.com>
2024-04-23 19:22:42 +0800

    add multimodal LLM BLIP-2 and InstructBLIP
    Former-commit-id: e1afbea68f9885ab4caebc948067c713cf612cf0

--- examples/mllm/sft_instructblip.sh
+++ examples/mllm/sft_instructblip.sh
@@ -14,7 +14,7 @@ CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --overwrite_output_dir \
     --cutoff_len 1024 \
     --preprocessing_num_workers 16 \
-    --per_device_train_batch_size 1 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
@@ -24,7 +24,7 @@ CUDA_VISIBLE_DEVICES=0 python src/train_bash.py \
     --eval_steps 100 \
     --evaluation_strategy steps \
     --load_best_model_at_end \
-    --learning_rate 5e-5 \
+    --learning_rate 1e-5 \
     --num_train_epochs 3.0 \
     --max_samples 3000 \
     --val_size 0.1 \
