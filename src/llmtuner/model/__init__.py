94ad744941b5d305892c38169a8478d0fb1a3019
BUAADreamer <1428195643@qq.com>
2024-04-25 21:08:32 +0800

    add some

--- src/llmtuner/model/__init__.py
+++ src/llmtuner/model/__init__.py
@@ -1,11 +1,10 @@
-from .loader import load_config, load_model, load_tokenizer, load_processor
+from .loader import load_config, load_model, load_tokenizer
 from .utils.misc import find_all_linear_modules, load_valuehead_params
 
 __all__ = [
     "load_config",
     "load_model",
     "load_tokenizer",
-    "load_processor",
     "load_valuehead_params",
     "find_all_linear_modules",
 ]
