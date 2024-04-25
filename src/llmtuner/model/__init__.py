00e2a272ef03fa33c6019f11fc0a588f6f0a82b9
BUAADreamer <1428195643@qq.com>
2024-04-25 08:20:41 +0800

    merge model part to the text stream
    Former-commit-id: b6fcb832ddaed4647d6f2b926f3dfccd47f3ea84

--- src/llmtuner/model/__init__.py
+++ src/llmtuner/model/__init__.py
@@ -1,10 +1,9 @@
-from .loader import load_config, load_model, load_tokenizer, load_mm_model
+from .loader import load_config, load_model, load_tokenizer, load_processor
 from .utils.misc import find_all_linear_modules, load_valuehead_params
 
 __all__ = [
     "load_config",
     "load_model",
-    "load_mm_model",
     "load_tokenizer",
     "load_processor",
     "load_valuehead_params",
