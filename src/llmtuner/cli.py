2fa7061552cc6ca3f10788cecd3968e40c5d6410
hiyouga <hiyouga@buaa.edu.cn>
2024-05-05 02:44:35 +0800

    add version and help to cli

--- src/llmtuner/cli.py
+++ src/llmtuner/cli.py
@@ -1,11 +1,25 @@
 import sys
 from enum import Enum, unique
 
+from . import __version__
 from .api.app import run_api
 from .chat.chat_model import run_chat
 from .eval.evaluator import run_eval
 from .train.tuner import export_model, run_exp
 from .webui.interface import run_web_demo, run_web_ui
+
+
+USAGE = """
+Usage:
+    llamafactory-cli api -h: launch an API server
+    llamafactory-cli chat -h: launch a chat interface in CLI
+    llamafactory-cli eval -h: do evaluation
+    llamafactory-cli export -h: merge LoRA adapters and export model
+    llamafactory-cli train -h: do training
+    llamafactory-cli webchat -h: launch a chat interface in Web UI
+    llamafactory-cli webui: launch LlamaBoard
+    llamafactory-cli version: show version info
+"""
 
 
 @unique
@@ -17,6 +31,8 @@ class Command(str, Enum):
     TRAIN = "train"
     WEBDEMO = "webchat"
     WEBUI = "webui"
+    VERSION = "version"
+    HELP = "help"
 
 
 def main():
@@ -35,5 +51,9 @@ def main():
         run_web_demo()
     elif command == Command.WEBUI:
         run_web_ui()
+    elif command == Command.VERSION:
+        print("Welcome to LLaMA Factory, version {}".format(__version__))
+    elif command == Command.HELP:
+        print(USAGE)
     else:
         raise NotImplementedError("Unknown command: {}".format(command))
