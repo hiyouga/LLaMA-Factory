5c5da41f45f1a404f7bad24e2e38346b6e5a5889
zhouwei <363232733@qq.com>
2024-05-06 13:29:59 +0800

    The training efficiency of the Ascend 910A has been significantly enhanced, leveraging the full computational power of the NPU (Neural Processing Unit) and the capabilities of torch_npu, a PyTorch library optimized for NPUs. This improvement has resulted in a remarkable tenfold increase in efficiency.

--- src/train.py
+++ src/train.py
@@ -1,3 +1,7 @@
+import os
+import torch
+import torch_npu
+from torch_npu.contrib import transfer_to_npu
 from llmtuner.train.tuner import run_exp
 
 
@@ -11,4 +15,6 @@ def _mp_fn(index):
 
 
 if __name__ == "__main__":
+    use_jit_compile = os.getenv('JIT_COMPILE', 'False').lower() in ['true', '1']
+    torch.npu.set_compile_mode(jit_compile=use_jit_compile)
     main()
