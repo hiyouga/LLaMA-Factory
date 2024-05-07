ebab655683d78f8039cf924ebe7c0cbebe68ab33
hiyouga <hiyouga@buaa.edu.cn>
2024-05-07 17:50:27 +0800

    fix #3602
    Former-commit-id: 1518b45490606ea200482da4737113c46985e8c5

--- examples/lora_multi_gpu/ds_zero3.sh
+++ examples/lora_multi_gpu/ds_zero3.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# ZeRO-3 enables weight sharding on multiple GPUs
 
-deepspeed --include "localhost:0,1,2,3" \
+NPROC_PER_NODE=4
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
+    --nproc_per_node $NPROC_PER_NODE \
+    --nnodes 1 \
+    --standalone \
     src/train.py examples/lora_multi_gpu/llama3_lora_sft_ds.yaml
