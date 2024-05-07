52cc6bce38f7502a7bbf95e6923b4f50c390f175
hiyouga <hiyouga@buaa.edu.cn>
2024-05-07 17:50:27 +0800

    fix #3602
    Former-commit-id: b0888262e371280f2b69ab529040b3a99632fc12

--- examples/full_multi_gpu/single_node.sh
+++ examples/full_multi_gpu/single_node.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-deepspeed --include "localhost:0,1,2,3" \
+NPROC_PER_NODE=4
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.run \
+    --nproc_per_node $NPROC_PER_NODE \
+    --nnodes 1 \
+    --standalone \
     src/train.py examples/full_multi_gpu/llama3_full_sft.yaml
