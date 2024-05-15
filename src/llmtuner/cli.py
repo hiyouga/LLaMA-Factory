1c910079d8544c433add2d949a8378822d1425c9
hiyouga <hiyouga@buaa.edu.cn>
2024-05-16 00:57:16 +0800

    release v0.7.1

--- src/llmtuner/cli.py
+++ src/llmtuner/cli.py
@@ -1,7 +1,6 @@
 import sys
 from enum import Enum, unique
 
-from . import __version__
 from .api.app import run_api
 from .chat.chat_model import run_chat
 from .eval.evaluator import run_eval
@@ -9,17 +8,34 @@ from .train.tuner import export_model, run_exp
 from .webui.interface import run_web_demo, run_web_ui
 
 
-USAGE = """
-Usage:
-    llamafactory-cli api -h: launch an API server
-    llamafactory-cli chat -h: launch a chat interface in CLI
-    llamafactory-cli eval -h: do evaluation
-    llamafactory-cli export -h: merge LoRA adapters and export model
-    llamafactory-cli train -h: do training
-    llamafactory-cli webchat -h: launch a chat interface in Web UI
-    llamafactory-cli webui: launch LlamaBoard
-    llamafactory-cli version: show version info
-"""
+USAGE = (
+    "-" * 70
+    + "\n"
+    + "| Usage:                                                             |\n"
+    + "|   llamafactory-cli api -h: launch an OpenAI-style API server       |\n"
+    + "|   llamafactory-cli chat -h: launch a chat interface in CLI         |\n"
+    + "|   llamafactory-cli eval -h: evaluate models                        |\n"
+    + "|   llamafactory-cli export -h: merge LoRA adapters and export model |\n"
+    + "|   llamafactory-cli train -h: train models                          |\n"
+    + "|   llamafactory-cli webchat -h: launch a chat interface in Web UI   |\n"
+    + "|   llamafactory-cli webui: launch LlamaBoard                        |\n"
+    + "|   llamafactory-cli version: show version info                      |\n"
+    + "-" * 70
+)
+
+VERSION = "0.7.1"
+
+WELCOME = (
+    "-" * 58
+    + "\n"
+    + "| Welcome to LLaMA Factory, version {}".format(VERSION)
+    + " " * (21 - len(VERSION))
+    + "|\n|"
+    + " " * 56
+    + "|\n"
+    + "| Project page: https://github.com/hiyouga/LLaMA-Factory |\n"
+    + "-" * 58
+)
 
 
 @unique
@@ -31,7 +47,7 @@ class Command(str, Enum):
     TRAIN = "train"
     WEBDEMO = "webchat"
     WEBUI = "webui"
-    VERSION = "version"
+    VER = "version"
     HELP = "help"
 
 
@@ -51,8 +67,8 @@ def main():
         run_web_demo()
     elif command == Command.WEBUI:
         run_web_ui()
-    elif command == Command.VERSION:
-        print("Welcome to LLaMA Factory, version {}".format(__version__))
+    elif command == Command.VER:
+        print(WELCOME)
     elif command == Command.HELP:
         print(USAGE)
     else:
