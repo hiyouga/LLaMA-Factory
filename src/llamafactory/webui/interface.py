694a05fd044bbbad107ca8fed5494460c78e1981
hiyouga <467089858@qq.com>
2024-05-17 00:08:56 +0800

    enable inbrowser in webui

--- src/llamafactory/webui/interface.py
+++ src/llamafactory/webui/interface.py
@@ -73,10 +73,10 @@ def create_web_demo() -> gr.Blocks:
 def run_web_ui() -> None:
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
-    create_ui().queue().launch(share=gradio_share, server_name=server_name)
+    create_ui().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
 
 
 def run_web_demo() -> None:
     gradio_share = os.environ.get("GRADIO_SHARE", "0").lower() in ["true", "1"]
     server_name = os.environ.get("GRADIO_SERVER_NAME", "0.0.0.0")
-    create_web_demo().queue().launch(share=gradio_share, server_name=server_name)
+    create_web_demo().queue().launch(share=gradio_share, server_name=server_name, inbrowser=True)
