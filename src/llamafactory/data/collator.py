66b5634ebf4ebe0f6891880d0d5189fea886a176
enji.zhou <enji.zhou@amh-group.com>
2024-05-17 13:09:17 +0800

    add kto
    Former-commit-id: ec51986cf70b0bdd79b8141e45916670fb97a08e

--- src/llamafactory/data/collator.py
+++ src/llamafactory/data/collator.py
@@ -49,3 +49,36 @@ class PairwiseDataCollatorWithPadding(DataCollatorForSeq2Seq):
         batch = super().__call__(concatenated_features)
         batch["labels"] = self._pad_labels(batch["input_ids"], label_positions)
         return batch
+
+@dataclass
+class KTODataCollatorWithPadding(DataCollatorForSeq2Seq):
+    r"""
+    Data collator for KTO data.
+    """
+    def __call__(self, features, return_tensors=None):
+        concatenated_features = []
+        kl_concatenated_features = []
+        tags = []
+        for feature in features:
+            concatenated_features.append(
+                {
+                    "input_ids": feature["input_ids"],
+                    "attention_mask": feature["attention_mask"],
+                    "labels": feature["labels"],
+                }
+            )
+            kl_concatenated_features.append(
+                {
+                    "input_ids": feature["kl_input_ids"],
+                    "attention_mask": feature["kl_attention_mask"],
+                    "labels": feature["kl_labels"],
+                }
+            )
+            tags.append(feature["tag"])
+        batch = super().__call__(concatenated_features)
+        kl_batch = super().__call__(kl_concatenated_features)
+        batch["KL_completion_input_ids"] = kl_batch["input_ids"]
+        batch["KL_completion_attention_mask"] = kl_batch["attention_mask"]
+        batch["kl_labels"] = kl_batch["labels"]
+        batch["tag"] = torch.tensor(tags)
+        return batch
