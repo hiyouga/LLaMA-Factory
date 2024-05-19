3578abc7a4c53c89b181ee3851afceb3e80d8512
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2024-05-20 00:29:31 +0800

    Update generating_args.py
    Former-commit-id: 861c146fa7d9cb5b99372464bd068c20fa36415d

--- src/llamafactory/hparams/generating_args.py
+++ src/llamafactory/hparams/generating_args.py
@@ -1,5 +1,5 @@
 from dataclasses import asdict, dataclass, field
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 
 @dataclass
@@ -46,10 +46,9 @@ class GeneratingArguments:
         default=1.0,
         metadata={"help": "Exponential penalty to the length that is used with beam-based generation."},
     )
-    system_message: str = field(
+    default_system: Optional[str] = field(
         default=None,
-        metadata={
-            "help": "System message is a message that the developer wrote to tell the bot how to interpret the conversation"},
+        metadata={"help": "Default system message to use in chat completion."},
     )
 
     def to_dict(self) -> Dict[str, Any]:
