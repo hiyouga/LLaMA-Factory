db745355bb0aaa5a132ccc52929c26eeddbe86c4
Jianbai Ye <jianbaiye@outlook.com>
2024-05-27 20:42:08 +0800

    add openchat-3.6-8B support
    Former-commit-id: b66f39d50d896d7597a1506e67ec210b31c9b700

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -756,6 +756,23 @@ _register_template(
     force_system=True,
 )
 
+_register_template(
+    name="openchat-3.6",
+    format_user=StringFormatter(
+        slots=[
+            (
+                "<|start_header_id|>GPT4 Correct User<|end_header_id|>\n\n{{content}}<|eot_id|>"
+                "<|start_header_id|>GPT4 Correct Assistant<|end_header_id|>\n\n"
+            )
+        ]
+    ),
+    format_system=StringFormatter(
+        slots=[{"bos_token"}, "<|start_header_id|>System<|end_header_id|>\n\n{{content}}<|eot_id|>"]
+    ),
+    stop_words=["<|eot_id|>"],
+    replace_eos=True,
+)
+
 
 _register_template(
     name="orion",
