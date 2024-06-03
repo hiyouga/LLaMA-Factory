af0995bb03bd135292975d99eb4bbd8122a74df4
hiyouga <467089858@qq.com>
2024-06-04 00:10:24 +0800

    fix abort in webui DDP mode
    Former-commit-id: 2187518762844472a96b72fbad4da15d8bc97bbd

--- src/llamafactory/webui/utils.py
+++ src/llamafactory/webui/utils.py
@@ -1,8 +1,10 @@
 import json
 import os
+import signal
 from datetime import datetime
 from typing import Any, Dict, List, Optional, Tuple
 
+import psutil
 from transformers.trainer_utils import get_last_checkpoint
 from yaml import safe_dump, safe_load
 
@@ -15,6 +17,18 @@ from .locales import ALERTS
 
 if is_gradio_available():
     import gradio as gr
+
+
+def abort_leaf_process(pid: int) -> None:
+    r"""
+    Aborts the leaf processes.
+    """
+    children = psutil.Process(pid).children()
+    if children:
+        for child in children:
+            abort_leaf_process(child.pid)
+    else:
+        os.kill(pid, signal.SIGABRT)
 
 
 def can_quantize(finetuning_type: str) -> "gr.Dropdown":
