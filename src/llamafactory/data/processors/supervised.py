fd7bd911a6573b9cd31b4a7b9dd1fdf699a39592
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2024-06-07 03:38:23 +0800

    Update supervised.py
    Former-commit-id: 788e8232fc4ed58ab2439a9bc2e38f64e12c6eb3

--- src/llamafactory/data/processors/supervised.py
+++ src/llamafactory/data/processors/supervised.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING, Any, Dict, List, Optional, Sequence, Tuple
 
 from ...extras.constants import IGNORE_INDEX
 from ...extras.logging import get_logger
-from .mm_utils import get_paligemma_token_type_ids, get_pixel_values, greedy_knapsack
+from .mm_utils import get_paligemma_token_type_ids, get_pixel_values
 
 
 if TYPE_CHECKING:
