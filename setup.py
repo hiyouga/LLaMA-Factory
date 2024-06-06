a4e1fcc881127f717440a2d7165c7339813e3c82
hiyouga <467089858@qq.com>
2024-06-06 16:59:18 +0800

    update readme
    Former-commit-id: cc331fa2d28afe081937c50ea83d63add21d4e3a

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requires():
 
 extra_require = {
     "torch": ["torch>=1.13.1"],
-    "torch_npu": ["torch==2.1.0", "torch-npu==2.1.0.post3", "decorator"],
+    "torch-npu": ["torch==2.1.0", "torch-npu==2.1.0.post3", "decorator"],
     "metrics": ["nltk", "jieba", "rouge-chinese"],
     "deepspeed": ["deepspeed>=0.10.0,<=0.14.0"],
     "bitsandbytes": ["bitsandbytes>=0.39.0"],
