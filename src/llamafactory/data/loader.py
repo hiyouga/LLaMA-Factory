149610c636bbb974e546d13fa302884ea65a6d38
hiyouga <467089858@qq.com>
2024-06-06 19:03:20 +0800

    fix ppo dataset bug #4012

--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -130,7 +130,7 @@ def get_dataset(
     model_args: "ModelArguments",
     data_args: "DataArguments",
     training_args: "Seq2SeqTrainingArguments",
-    stage: Literal["pt", "sft", "rm", "kto"],
+    stage: Literal["pt", "sft", "rm", "ppo", "kto"],
     tokenizer: "PreTrainedTokenizer",
     processor: Optional["ProcessorMixin"] = None,
 ) -> Union["Dataset", "IterableDataset"]:
