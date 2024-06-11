6625bf6b332a52d09be21c0de3b989468d1ecf6c
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2024-06-11 17:02:14 +0800

    Update pretrain.py
    Former-commit-id: e2317b2a84149e39fddfd6366be3de23dfb71f82

--- src/llamafactory/data/processors/pretrain.py
+++ src/llamafactory/data/processors/pretrain.py
@@ -12,7 +12,7 @@ def preprocess_pretrain_dataset(
     examples: Dict[str, List[Any]], tokenizer: "PreTrainedTokenizer", data_args: "DataArguments"
 ) -> Dict[str, List[List[int]]]:
     # build grouped texts with format `X1 X2 X3 ...` if packing is enabled
-    eos_token = '<|end_of_text|>' if data_args.template == 'llama3' else  tokenizer.eos_token
+    eos_token = "<|end_of_text|>" if data_args.template == "llama3" else tokenizer.eos_token
     text_examples = [messages[0]["content"] + eos_token for messages in examples["prompt"]]
 
     if not data_args.packing:
