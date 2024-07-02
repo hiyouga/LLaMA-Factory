cc31014002e47169080f958595a8b87d559eec78
hiyouga <467089858@qq.com>
2024-07-02 22:23:08 +0800

    improve rlhf
    Former-commit-id: c47ab6c07287fb260ea49b8b7af46bdd416f88f7

--- src/llamafactory/data/parser.py
+++ src/llamafactory/data/parser.py
@@ -31,31 +31,31 @@ class DatasetAttr:
     Dataset attributes.
     """
 
-    """ basic configs """
+    # basic configs
     load_from: Literal["hf_hub", "ms_hub", "script", "file"]
     dataset_name: str
     formatting: Literal["alpaca", "sharegpt"] = "alpaca"
     ranking: bool = False
-    """ extra configs """
+    # extra configs
     subset: Optional[str] = None
     folder: Optional[str] = None
     num_samples: Optional[int] = None
-    """ common columns """
+    # common columns
     system: Optional[str] = None
     tools: Optional[str] = None
     images: Optional[str] = None
-    """ rlhf columns """
+    # rlhf columns
     chosen: Optional[str] = None
     rejected: Optional[str] = None
     kto_tag: Optional[str] = None
-    """ alpaca columns """
+    # alpaca columns
     prompt: Optional[str] = "instruction"
     query: Optional[str] = "input"
     response: Optional[str] = "output"
     history: Optional[str] = None
-    """ sharegpt columns """
+    # sharegpt columns
     messages: Optional[str] = "conversations"
-    """ sharegpt tags """
+    # sharegpt tags
     role_tag: Optional[str] = "from"
     content_tag: Optional[str] = "value"
     user_tag: Optional[str] = "human"
