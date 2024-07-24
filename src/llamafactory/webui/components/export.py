c68fe0c53f8d46a613acf16ae151d742e88e12ab
hiyouga <467089858@qq.com>
2024-07-24 21:11:51 +0800

    update webui
    Former-commit-id: 463edec1b1c1345afc791e225deb33f118f3582e

--- src/llamafactory/webui/components/export.py
+++ src/llamafactory/webui/components/export.py
@@ -104,7 +104,7 @@ def save_model(
 
 def create_export_tab(engine: "Engine") -> Dict[str, "Component"]:
     with gr.Row():
-        export_size = gr.Slider(minimum=1, maximum=100, value=1, step=1)
+        export_size = gr.Slider(minimum=1, maximum=100, value=5, step=1)
         export_quantization_bit = gr.Dropdown(choices=["none"] + GPTQ_BITS, value="none")
         export_quantization_dataset = gr.Textbox(value="data/c4_demo.json")
         export_device = gr.Radio(choices=["cpu", "auto"], value="cpu")
