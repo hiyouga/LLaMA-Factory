09f67e554682b6b1be2af79334f35c701f88b24c
qianhao0713 <475483052@qq.com>
2024-09-03 15:35:25 +0800

    pycuda -> cudart

--- src/llamafactory/easy_context/dist_flash_attn/offload_buffer.py
+++ src/llamafactory/easy_context/dist_flash_attn/offload_buffer.py
@@ -1,7 +1,6 @@
-import pycuda
-import pycuda.driver as drv
-import pycuda.autoinit
-import numpy as np
+import cuda
+import cuda.cudart
+import torch
 
 class OffloadBuffer:
     
@@ -14,26 +13,29 @@ class OffloadBuffer:
         if self.allocated:
             return
         self.layer_num = num_layers
-        self.gpu_layer_num = int(num_layers * self.offload_percent)
-        self.cpu_layer_num = num_layers - self.gpu_layer_num
+        self.cpu_layer_num = int(num_layers * self.offload_percent)
+        self.gpu_layer_num = num_layers - self.cpu_layer_num
         self.gpu_buffer = [None for _ in range(self.gpu_layer_num)]
-        self.cpu_buffer = drv.pagelocked_empty([self.cpu_layer_num] + shape, dtype=np.float16)
+        self.cpu_buffer = [torch.empty([self.cpu_layer_num] + shape, dtype=torch.bfloat16) for _ in range(self.cpu_layer_num)]
         bs, num_heads, seq_len, emb_size = shape
         shape_h = [bs, seq_len, num_heads * emb_size]
         shape_a = [bs, seq_len]
         self.hidden_state_gpu_buffer = [None for _ in range(self.gpu_layer_num)]
-        self.hidden_state_cpu_buffer = drv.pagelocked_empty([self.cpu_layer_num] + shape_h, dtype=np.float16)
+        self.hidden_state_cpu_buffer = [torch.empty([self.cpu_layer_num] + shape_h, dtype=torch.bfloat16) for _ in range(self.cpu_layer_num)]
         self.position_id_gpu_buffer = [None for _ in range(self.gpu_layer_num)]
-        self.position_id_cpu_buffer = drv.pagelocked_empty([self.cpu_layer_num] + shape_a, dtype=np.float16)
-        self.d2h_stream = drv.Stream()
-        self.h2d_streams = [drv.Stream() for _ in range(self.gpu_layer_num)]
+        self.position_id_cpu_buffer = [torch.empty([self.cpu_layer_num] + shape_a, dtype=torch.bfloat16) for _ in range(self.cpu_layer_num)]
+        _, self.d2h_stream = cuda.cudart.cudaStreamCreate()
+        self.h2d_streams = []
+        for i in range(self.gpu_layer_num):
+            _, h2d_stream = cuda.cudart.cudaStreamCreate()
+            self.h2d_streams.append(h2d_stream)
         self.allocated = True
 
     def save_flash_attn_out(self, layer_idx, out):
         if layer_idx < 0:
             layer_idx = self.layer_num + layer_idx
         if layer_idx < self.cpu_layer_num:
-            drv.memcpy_dtoh_async(self.cpu_buffer[layer_idx], out.data_ptr(), self.d2h_stream)
+            _ = cuda.cudart.cudaMemcpyAsync(self.cpu_buffer[layer_idx].data_ptr(), out.data_ptr(), out.nelement() * out.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost, self.d2h_stream)
         else:
             idx = layer_idx - self.cpu_layer_num
             self.gpu_buffer[idx] = out
@@ -44,8 +46,8 @@ class OffloadBuffer:
         hidden_state = hs[0]
         position_id = hs[1]
         if layer_idx < self.cpu_layer_num:
-            drv.memcpy_dtoh_async(self.hidden_state_cpu_buffer[layer_idx], hidden_state.data_ptr(), self.d2h_stream)
-            drv.memcpy_dtoh_async(self.position_id_cpu_buffer[layer_idx], position_id.data_ptr(), self.d2h_stream)
+            _ = cuda.cudart.cudaMemcpyAsync(self.hidden_state_cpu_buffer[layer_idx].data_ptr(), hidden_state.data_ptr(), hidden_state.nelement() * hidden_state.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost, self.d2h_stream)
+            _ = cuda.cudart.cudaMemcpyAsync(self.position_id_cpu_buffer[layer_idx].data_ptr(), position_id.data_ptr(), position_id.nelement() * position_id.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyDeviceToHost, self.d2h_stream)
         else:
             idx = layer_idx - self.cpu_layer_num
             self.hidden_state_gpu_buffer[idx] = hidden_state
@@ -57,7 +59,7 @@ class OffloadBuffer:
         if layer_idx >= self.cpu_layer_num:
             return self.gpu_buffer[layer_idx - self.cpu_layer_num]
         idx = self.gpu_layer_num -1 - (self.cpu_layer_num - 1 - layer_idx) % self.gpu_layer_num
-        self.h2d_streams[idx].synchronize()
+        _ = cuda.cudart.cudaStreamSynchronize(self.h2d_streams[idx])
         return self.gpu_buffer[idx]
     
     def get_hidden_states(self, layer_idx):
@@ -66,14 +68,14 @@ class OffloadBuffer:
         if layer_idx >= self.cpu_layer_num:
             return self.hidden_state_gpu_buffer[layer_idx - self.cpu_layer_num], self.position_id_gpu_buffer[layer_idx - self.cpu_layer_num]
         idx = self.gpu_layer_num -1 - (self.cpu_layer_num - 1 - layer_idx) % self.gpu_layer_num
-        self.h2d_streams[idx].synchronize()
+        _ = cuda.cudart.cudaStreamSynchronize(self.h2d_streams[idx])
         return self.hidden_state_gpu_buffer[idx], self.position_id_gpu_buffer[idx]
 
     def free_layer_gpu_buffer(self, layer_idx):
         if layer_idx < 0:
             layer_idx = self.layer_num + layer_idx
         if layer_idx == self.layer_num - 1:
-            self.d2h_stream.synchronize()
+            _ = cuda.cudart.cudaStreamSynchronize(self.d2h_stream)
         cpu_layer_idx = layer_idx - self.gpu_layer_num
         if layer_idx >= self.cpu_layer_num:
             idx = layer_idx - self.cpu_layer_num
@@ -85,8 +87,19 @@ class OffloadBuffer:
             self.hidden_state_gpu_buffer[idx] = None
             self.position_id_gpu_buffer[idx] = None
             return
-        drv.memcpy_htod_async(self.gpu_buffer[idx].data_ptr(), self.cpu_buffer[cpu_layer_idx], self.h2d_streams[idx])
-        drv.memcpy_htod_async(self.hidden_state_gpu_buffer[idx].data_ptr(), self.hidden_state_cpu_buffer[cpu_layer_idx], self.h2d_streams[idx])
-        drv.memcpy_htod_async(self.position_id_gpu_buffer[idx].data_ptr(), self.position_id_cpu_buffer[cpu_layer_idx], self.h2d_streams[idx])
+        cb = self.cpu_buffer[cpu_layer_idx]
+        hcb = self.hidden_state_cpu_buffer[cpu_layer_idx]
+        pcb = self.position_id_cpu_buffer[cpu_layer_idx]
+        gb = self.gpu_buffer[idx]
+        hgb = self.hidden_state_gpu_buffer[idx]
+        pgb = self.position_id_gpu_buffer[idx]
+        _ = cuda.cudart.cudaMemcpyAsync(gb.data_ptr(), cb.data_ptr(), gb.nelement() * gb.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyHostToDevice, self.h2d_streams[idx])
+        _ = cuda.cudart.cudaMemcpyAsync(hgb.data_ptr(), hcb.data_ptr(), hgb.nelement() * hgb.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyHostToDevice, self.h2d_streams[idx])
+        _ = cuda.cudart.cudaMemcpyAsync(pgb.data_ptr(), pcb.data_ptr(), pgb.nelement() * pgb.element_size(), cuda.cudart.cudaMemcpyKind.cudaMemcpyHostToDevice, self.h2d_streams[idx])
+        
+    def __del__(self):
+        cuda.cudart.cudaStreamDestroy(self.d2h_stream)
+        for i in range(self.gpu_layer_num):
+            cuda.cudart.cudaStreamDestroy(self.h2d_streams[i])
         
 offload_buffer = None
