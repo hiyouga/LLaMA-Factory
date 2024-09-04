faf06bffe9e58ff49bfc26a05fa267b20365a665
liudan <403644786@qq.com>
2024-09-04 23:10:05 +0800

    增加了对minicpm3.0的适配'
    Former-commit-id: 4ad3a761af2452ef3f6c61190b7e47c9ea5227b9

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -558,7 +558,13 @@ _register_template(
     format_user=StringFormatter(slots=["<用户>{{content}}<AI>"]),
     format_prefix=EmptyFormatter(slots=[{"bos_token"}]),
 )
-
+_register_template(
+    name="cpm3",
+    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
+    format_prefix=EmptyFormatter(slots=[{"bos_token"}]),
+    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
+    stop_words=["<|im_end|>"],
+)
 
 _register_template(
     name="dbrx",
