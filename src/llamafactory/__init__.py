2c61942632a9358fc40b91aced5865e7a2f6681d
hiyouga <467089858@qq.com>
2024-09-09 22:51:08 +0800

    update accelerate ver for schedule_free optimizers
    Former-commit-id: 2de74e79049ce8e50f605f649275b1dbfb899c8c

--- src/llamafactory/__init__.py
+++ src/llamafactory/__init__.py
@@ -22,7 +22,7 @@ Dependency graph:
   main:
     transformers>=4.41.2,<=4.45.0
     datasets>=2.16.0,<=2.21.0
-    accelerate>=0.30.1,<=0.33.0
+    accelerate>=0.30.1,<=0.34.2
     peft>=0.11.1,<=0.12.0
     trl>=0.8.6,<=0.9.6
   attention:
