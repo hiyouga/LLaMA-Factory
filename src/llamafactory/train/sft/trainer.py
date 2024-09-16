ee7e0a7979bfbb70b62ffee32f261e7167094b6b
Billy Cao <aliencaocao@gmail.com>
2024-09-16 23:26:50 +0800

    Correctly pass gen_kwarg to eval during model runs

--- src/llamafactory/train/sft/trainer.py
+++ src/llamafactory/train/sft/trainer.py
@@ -49,7 +49,7 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
     """
 
     def __init__(
-        self, finetuning_args: "FinetuningArguments", processor: Optional["ProcessorMixin"], **kwargs
+        self, finetuning_args: "FinetuningArguments", processor: Optional["ProcessorMixin"], gen_kwargs: Optional[Dict[str, Any]], **kwargs
     ) -> None:
         if is_transformers_version_greater_than("4.46"):
             kwargs["processing_class"] = kwargs.pop("tokenizer")
@@ -58,6 +58,7 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
 
         super().__init__(**kwargs)
         self.finetuning_args = finetuning_args
+        self._gen_kwargs = gen_kwargs
 
         if processor is not None:
             self.add_callback(SaveProcessorCallback(processor))
