97271545679cb97a0682fe86c9df2727dca026cb
Johnny <johnnync13@gmail.com>
2024-10-06 20:34:19 +0200

    Update parser.py
    Former-commit-id: 6c1aef55604649a956fe928d89280626923815b8

--- src/llamafactory/hparams/parser.py
+++ src/llamafactory/hparams/parser.py
@@ -123,7 +123,7 @@ def _check_extra_dependencies(
         require_version("mixture-of-depth>=1.1.6", "To fix: pip install mixture-of-depth>=1.1.6")
 
     if model_args.infer_backend == "vllm":
-        require_version("vllm>=0.4.3,<=0.6.0", "To fix: pip install vllm>=0.4.3,<=0.6.0")
+        require_version("vllm>=0.4.3,<=0.6.4", "To fix: pip install vllm>=0.4.3,<=0.6.4")
 
     if finetuning_args.use_galore:
         require_version("galore_torch", "To fix: pip install galore_torch")
