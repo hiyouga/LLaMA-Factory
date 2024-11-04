1a8c3a9928ea2b14dffb986339f9af9a8a0e1470
Choi-jun9803 <choiyj9803@gmail.com>
2024-11-04 12:47:51 +0900

    remove other source

--- examples/extras/fsdp_qlora/train.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# DO NOT use GPTQ/AWQ model in FSDP+QLoRA
-
-CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
-    --config_file examples/accelerate/fsdp_config.yaml \
-    src/train.py examples/extras/fsdp_qlora/llama3_lora_sft.yaml
--- examples/extras/pissa/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python scripts/pissa_init.py \
-    --model_name_or_path meta-llama/Meta-Llama-3-8B-Instruct \
-    --output_dir models/llama3-8b-pissa
