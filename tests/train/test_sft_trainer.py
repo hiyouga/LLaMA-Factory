8524dcaa4a8ce067575b4c1ef3ecca7a85a2d5a1
hiyouga <hiyouga@buaa.edu.cn>
2024-12-19 12:16:30 +0000

    fix #6391
    Former-commit-id: d4c1fda1ad19e73484d8d51d81e490cdb8781955

--- tests/train/test_sft_trainer.py
+++ tests/train/test_sft_trainer.py
@@ -58,7 +58,11 @@ class DataCollatorWithVerbose(DataCollatorWithPadding):
 @pytest.mark.parametrize("disable_shuffling", [False, True])
 def test_shuffle(disable_shuffling: bool):
     model_args, data_args, training_args, finetuning_args, _ = get_train_args(
-        {"output_dir": f"dummy_dir/{disable_shuffling}", "disable_shuffling": disable_shuffling, **TRAIN_ARGS}
+        {
+            "output_dir": os.path.join("output", f"shuffle{str(disable_shuffling).lower()}"),
+            "disable_shuffling": disable_shuffling,
+            **TRAIN_ARGS,
+        }
     )
     tokenizer_module = load_tokenizer(model_args)
     tokenizer = tokenizer_module["tokenizer"]
