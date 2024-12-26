95bb98634b9fa3c9b880e2ec8c607502823a5a9f
ylfeng <ylfeng@ir.hit.edu.cn>
2024-12-27 00:43:59 +0800

    fix tests broken

--- src/llamafactory/data/data_utils.py
+++ src/llamafactory/data/data_utils.py
@@ -125,4 +125,6 @@ def continuous_messages(messages: Sequence[Dict[str, str]], dataset_attr: "Datas
     if len(waiting_message) > 0:
         append_waiting_message()
 
+    if len(new_messages) == len(messages):
+        return messages
     return new_messages
