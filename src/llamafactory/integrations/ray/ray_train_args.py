88e9badcbb5d23e1bacdeaa7e3c987a59f7b4e2d
Eric Tang <erictang000@gmail.com>
2025-01-06 23:55:56 +0000

    run style check
    Former-commit-id: 5ec33baf5f95df9fa2afe5523c825d3eda8a076b

--- src/llamafactory/integrations/ray/ray_train_args.py
+++ src/llamafactory/integrations/ray/ray_train_args.py
@@ -3,14 +3,23 @@ from typing import Any, Dict, Literal, Optional
 
 from .ray_utils import should_use_ray
 
+
 @dataclass
 class RayTrainArguments:
     r"""
     Arguments pertaining to the Ray training.
     """
-    resources_per_worker: Optional[Dict[str, Any]] = field(default_factory=lambda: {"GPU": 1}, metadata={"help": "The resources per worker for Ray training. Default is to use 1 GPU per worker."})
-    num_workers: Optional[int] = field(default=1, metadata={"help": "The number of workers for Ray training. Default is 1 worker."})
-    placement_strategy: Optional[Literal["SPREAD", "PACK", "STRICT_SPREAD", "STRICT_PACK"]] = field(default="PACK", metadata={"help": "The placement strategy for Ray training. Default is PACK."})
+
+    resources_per_worker: Optional[Dict[str, Any]] = field(
+        default_factory=lambda: {"GPU": 1},
+        metadata={"help": "The resources per worker for Ray training. Default is to use 1 GPU per worker."},
+    )
+    num_workers: Optional[int] = field(
+        default=1, metadata={"help": "The number of workers for Ray training. Default is 1 worker."}
+    )
+    placement_strategy: Optional[Literal["SPREAD", "PACK", "STRICT_SPREAD", "STRICT_PACK"]] = field(
+        default="PACK", metadata={"help": "The placement strategy for Ray training. Default is PACK."}
+    )
 
     @property
     def use_ray(self) -> bool:
@@ -19,4 +28,3 @@ class RayTrainArguments:
         This prevents manual setting of use_ray.
         """
         return should_use_ray()
-    
