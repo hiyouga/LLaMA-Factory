deacc00b1226ca3d53bf7bb1231cf276eaa8296b
Haian Huang(深度眸) <1286304229@qq.com>
2025-01-14 18:07:27 +0800

    Support InternLM3 Dense 8B Model (#6640)
    * support internlm3
    
    * update
    
    * update
    
    * update
    
    * add hint

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -732,6 +732,14 @@ _register_template(
     stop_words=["<|im_end|>"],
 )
 
+_register_template(
+    name="intern3",
+    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
+    format_assistant=StringFormatter(slots=["{{content}}<|im_end|>\n"]),
+    format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
+    format_prefix=EmptyFormatter(slots=[{"bos_token"}]),
+    stop_words=["<|im_end|>"]
+)
 
 _register_template(
     name="llama2",
