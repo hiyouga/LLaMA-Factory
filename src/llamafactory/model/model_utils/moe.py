7bf09abf1c4d971cda33daed933c75f391e79294
hoshi-hiyouga <hiyouga@buaa.edu.cn>
2025-01-17 13:46:09 +0800

    fix qwen2 moe (#6684)

--- src/llamafactory/model/model_utils/moe.py
+++ src/llamafactory/model/model_utils/moe.py
@@ -61,7 +61,7 @@ def add_z3_leaf_module(model: "PreTrainedModel") -> None:
 
         _set_z3_leaf_modules(model, [MixtralSparseMoeBlock])
 
-    if model_type == "qwen2moe":
+    if model_type == "qwen2_moe":
         from transformers.models.qwen2_moe.modeling_qwen2_moe import Qwen2MoeSparseMoeBlock
 
         _set_z3_leaf_modules(model, [Qwen2MoeSparseMoeBlock])
