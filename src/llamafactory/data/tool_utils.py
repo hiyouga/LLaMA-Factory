70d1f1fe85cb70e7401eaea925a70879c292abfc
Yueqi Song <141804823+yueqis@users.noreply.github.com>
2025-02-02 14:05:06 -0500

    Update tool_utils.py

--- src/llamafactory/data/tool_utils.py
+++ src/llamafactory/data/tool_utils.py
@@ -56,7 +56,7 @@ QWEN_TOOL_PROMPT = (
     "You are provided with function signatures within <tools></tools> XML tags:\n<tools>{tool_text}"
     "\n</tools>\n\nFor each function call, return a json object with function name and arguments within "
     """<tool_call></tool_call> XML tags:\n<tool_call>\n{{"name": <function-name>, """
-    """"arguments": <args-json-object>}}\n</tool_call><|im_end|>\n"""
+    """"arguments": <args-json-object>}}\n</tool_call>"""
 )
 
 
