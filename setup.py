f50c4638000e7069abeddff263f00e9a9c02187f
Guo, Quan <1118270+guoquan@users.noreply.github.com>
2025-03-23 00:34:26 +0800

    feat: Add transformer version requirement for sglang

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ extra_require = {
     "awq": ["autoawq"],
     "aqlm": ["aqlm[gpu]>=1.1.0"],
     "vllm": ["vllm>=0.4.3,<=0.7.3"],
-    "sglang": ["sglang>=0.4.4"],
+    "sglang": ["sglang>=0.4.4", "transformers==4.48.3"],
     "galore": ["galore-torch"],
     "apollo": ["apollo-torch"],
     "badam": ["badam>=1.2.1"],
