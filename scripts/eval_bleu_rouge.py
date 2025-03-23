a0883d6ee56f4084c77f8f6ae13fd510379219fc
SnowFox4004 <manba@out>
2025-03-23 16:10:57 +0800

    update: 使用datasets库的多进程加速处理

--- scripts/eval_bleu_rouge.py
+++ scripts/eval_bleu_rouge.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import sys
 import time
 
@@ -6,14 +7,68 @@ import time
 try:
     import jieba
     import jsonlines
+    from datasets import load_dataset
     from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu
     from rouge_chinese import Rouge
-except Exception:
-    print("Failed to start, please install these requirements:\n\t pip install jieba jsonlines nltk rouge_chinese")
+except Exception as err:
+    print(
+        f"Failed to start, please install these requirements:\n{err}\n\t pip install jieba jsonlines nltk rouge_chinese"
+    )
     sys.exit(1)
+
+# 关掉输出
+jieba.setLogLevel(logging.CRITICAL)
+jieba.initialize()
+
+
+def compute_metrics(sample):
+    hypothesis = list(jieba.cut(sample["predict"]))
+    reference = list(jieba.cut(sample["label"]))
+
+    bleu_score = sentence_bleu(
+        [list(sample["label"])],
+        list(sample["predict"]),
+        smoothing_function=SmoothingFunction().method3,
+    )
+
+    if len(" ".join(hypothesis).split()) == 0 or len(" ".join(reference).split()) == 0:
+        result = {"rouge-1": {"f": 0.0}, "rouge-2": {"f": 0.0}, "rouge-l": {"f": 0.0}}
+    else:
+        rouge = Rouge()
+        scores = rouge.get_scores(" ".join(hypothesis), " ".join(reference))
+        result = scores[0]
+
+    metric_result = {}
+    for k, v in result.items():
+        metric_result[k] = round(v["f"] * 100, 4)
+    metric_result["bleu-4"] = round(bleu_score * 100, 4)
+
+    return metric_result
 
 
 def main(filename: str):
+    start_time = time.time()
+    dataset = load_dataset("json", data_files=filename, split="train")
+
+    # 多进程
+    dataset = dataset.map(compute_metrics, num_proc=6, remove_columns=dataset.column_names)
+    score_dict = dataset.to_dict()
+
+    average_score = {}
+    for task, scores in sorted(score_dict.items(), key=lambda x: x[0]):
+        print(f"{task}: {sum(scores) / len(scores):.4f}")
+        average_score[task] = sum(scores) / len(scores)
+
+    json.dump(
+        average_score,
+        open("predictions_score.json", "w+", encoding="utf-8"),
+        indent=4,
+    )
+    print(f"\nDone in {time.time() - start_time: .3f}s \nscore file saved to predictions_score.json")
+    return average_score
+
+
+def deprecated_main(filename: str):
     start_time = time.time()
     predictions = []
     references = []
@@ -53,7 +108,11 @@ def main(filename: str):
         print(f"{task}: {sum(scores) / len(scores):.4f}")
         average_score[task] = sum(scores) / len(scores)
 
-    json.dump(average_score, open("predictions_score.json", "w+", encoding="utf-8"), indent=4)
+    json.dump(
+        average_score,
+        open("predictions_score_legacy.json", "w+", encoding="utf-8"),
+        indent=4,
+    )
     print(f"\nDone in {time.time() - start_time: .3f}s \nscore file saved to predictions_score.json")
 
 
@@ -63,3 +122,4 @@ if __name__ == "__main__":
         sys.exit(1)
 
     main(sys.argv[1])
+    # main_legacy(sys.argv[1])
