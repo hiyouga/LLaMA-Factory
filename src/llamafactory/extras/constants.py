ee6df118c3e96c9245597192ec0f6c31de21d5f2
Robert <belanecrobert22@gmail.com>
2025-03-31 11:29:28 +0200

    added new dataset

--- src/llamafactory/extras/constants.py
+++ src/llamafactory/extras/constants.py
@@ -56,13 +56,13 @@ LAYERNORM_NAMES = {"norm", "ln"}
 
 LLAMABOARD_CONFIG = "llamaboard_config.yaml"
 
-METHODS = ["full", "freeze", "lora"]
+METHODS = ["full", "freeze", "lora", "prompt tuning", "prefix tuning", "p-tuning", "DePT", "ADePT", "MPT", "ATTEMPT", "adapters", "adapter fusion", "IA3", "BitFit"]
 
 MOD_SUPPORTED_MODELS = {"bloom", "falcon", "gemma", "llama", "mistral", "mixtral", "phi", "starcoder2"}
 
 MULTIMODAL_SUPPORTED_MODELS = set()
 
-PEFT_METHODS = {"lora"}
+PEFT_METHODS = {"lora", "prompt tuning", "prefix tuning", "p-tuning", "DePT", "ADePT", "MPT", "ATTEMPT", "adapters", "adapter fusion", "IA3", "BitFit"}
 
 RUNNING_LOG = "running_log.txt"
 
