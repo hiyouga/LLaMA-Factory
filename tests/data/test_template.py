ee4e8da03e34281e763c337204bddc5aa63a2efe
hiyouga <hiyouga@buaa.edu.cn>
2025-05-20 00:56:46 +0800

    fix tests

--- tests/data/test_template.py
+++ tests/data/test_template.py
@@ -135,7 +135,7 @@ def test_reasoning_encode_oneturn(use_fast: bool, messages: list[dict[str, str]]
     prompt_ids, answer_ids = template.encode_oneturn(tokenizer, messages)
     prompt_str = (
         f"<|im_start|>user\n{messages[0]['content']}<|im_end|>\n"
-        f"<|im_start|>assistant\n{messages[1]['content']}<|im_end|>\n"
+        f"<|im_start|>assistant\n{MESSAGES[1]['content']}<|im_end|>\n"
         f"<|im_start|>user\n{messages[2]['content']}<|im_end|>\n"
         "<|im_start|>assistant\n"
     )
@@ -281,7 +281,7 @@ def test_qwen2_5_template(use_fast: bool):
 def test_qwen3_template(use_fast: bool, messages: list[dict[str, str]]):
     prompt_str = (
         f"<|im_start|>user\n{messages[0]['content']}<|im_end|>\n"
-        f"<|im_start|>assistant\n{messages[1]['content']}<|im_end|>\n"
+        f"<|im_start|>assistant\n{MESSAGES[1]['content']}<|im_end|>\n"
         f"<|im_start|>user\n{messages[2]['content']}<|im_end|>\n"
         "<|im_start|>assistant\n"
     )
