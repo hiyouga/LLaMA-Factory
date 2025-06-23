477bed6f899b2e2efc2b517742c13036db84bda0
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-06-23 17:56:48 +0800

    [model] add kimi vl 2506 (#8432)

--- src/llamafactory/data/template.py
+++ src/llamafactory/data/template.py
@@ -916,14 +916,13 @@ register_template(
 )
 
 
+# copied from chatml template
 register_template(
     name="falcon_h1",
-    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n"]),
+    format_user=StringFormatter(slots=["<|im_start|>user\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
     format_assistant=StringFormatter(slots=["{{content}}<|im_end|>\n"]),
     format_system=StringFormatter(slots=["<|im_start|>system\n{{content}}<|im_end|>\n"]),
-    format_function=FunctionFormatter(slots=["{{content}}<|im_end|>\n"], tool_format="default"),
-    format_observation=StringFormatter(slots=["<|im_start|>tool\n{{content}}<|im_end|>\n"]),
-    format_tools=ToolFormatter(tool_format="default"),
+    format_observation=StringFormatter(slots=["<|im_start|>tool\n{{content}}<|im_end|>\n<|im_start|>assistant\n"]),
     format_prefix=EmptyFormatter(slots=[{"bos_token"}]),
     stop_words=["<|im_end|>", "<|end_of_text|>"],
 )
