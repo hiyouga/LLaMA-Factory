68655b718b6b365089e269296a57f98bc65482eb
Robert <belanecrobert22@gmail.com>
2025-06-24 13:06:55 +0200

    license

--- tests/check_license.py
+++ tests/check_license.py
@@ -1,3 +1,17 @@
+# Copyright 2025 the PEFTFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Copyright 2025 the LlamaFactory team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -31,7 +45,7 @@ def main():
                 continue
 
             print(f"Check license: {path}")
-            assert all(keyword in file_content[0] for keyword in KEYWORDS), f"File {path} does not contain license."
+            assert any((all(keyword in file_content[0] for keyword in KEYWORDS), "PEFTFactory" in file_content[0])), f"File {path} does not contain license."
 
 
 if __name__ == "__main__":
