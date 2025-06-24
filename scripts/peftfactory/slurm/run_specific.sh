ab75ae4fa1880dfd1f12df23256ec15298eb24fb
Robert <belanecrobert22@gmail.com>
2025-06-24 12:45:34 +0200

    quality and style

--- scripts/peftfactory/slurm/run_specific.sh
+++ scripts/peftfactory/slurm/run_specific.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+# Copyright 2025 the PEFTFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # configs=(prompt-tuning_llama-3-8b-instruct_mnli_1744902585 lntuning_gemma-3-1b-it_mnli_1744902583 prompt-tuning_mistral-7b-instruct_mnli_1744902589 lora_llama-3-8b-instruct_mnli_1744902586 lntuning_llama-3-8b-instruct_mnli_1744902587 ia3_mistral-7b-instruct_mnli_1744902588)
 configs=(prompt-tuning_llama-3-8b-instruct_train_stsb_1745333591)
 
