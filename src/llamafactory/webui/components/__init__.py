30157d2bba92886ad15ad77745bd747114d31f08
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-06-25 21:59:58 +0800

    [webui] upgrade webui and fix api (#8460)

--- src/llamafactory/webui/components/__init__.py
+++ src/llamafactory/webui/components/__init__.py
@@ -15,6 +15,7 @@
 from .chatbot import create_chat_box
 from .eval import create_eval_tab
 from .export import create_export_tab
+from .footer import create_footer
 from .infer import create_infer_tab
 from .top import create_top
 from .train import create_train_tab
@@ -24,6 +25,7 @@ __all__ = [
     "create_chat_box",
     "create_eval_tab",
     "create_export_tab",
+    "create_footer",
     "create_infer_tab",
     "create_top",
     "create_train_tab",
