46cfb5e1e2a44d397fe6e81cce4f0680ac42610d
xzp <502312237@qq.com>
2025-07-29 11:29:00 +0000

    优化效率

--- src/llamafactory/hparams/training_args.py
+++ src/llamafactory/hparams/training_args.py
@@ -72,6 +72,7 @@ class RayArguments:
             elif self.ray_storage_filesystem == "gs" or self.ray_storage_filesystem == "gcs":
                 self.ray_storage_filesystem = fs.GcsFileSystem()
 
+
 @dataclass
 class TrainingArguments(RayArguments, Seq2SeqTrainingArguments):
     r"""Arguments pertaining to the trainer."""
