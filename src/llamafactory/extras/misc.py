90a47ce892915914e20272a9f0b6835c757d9a6a
Dean Daskalantonakis <dd@neuralfabric.ai>
2025-08-16 13:22:25 -0500

    upgrade transformers to support smollm3 & gpt oss

--- src/llamafactory/extras/misc.py
+++ src/llamafactory/extras/misc.py
@@ -94,7 +94,7 @@ def check_version(requirement: str, mandatory: bool = False) -> None:
 
 def check_dependencies() -> None:
     r"""Check the version of the required packages."""
-    check_version("transformers>=4.49.0,<=4.52.4,!=4.52.0")
+    check_version("transformers>=4.55.2")
     check_version("datasets>=2.16.0,<=3.6.0")
     check_version("accelerate>=1.3.0,<=1.7.0")
     check_version("peft>=0.14.0,<=0.15.2")
