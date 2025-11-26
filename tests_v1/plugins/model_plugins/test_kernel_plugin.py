442e8a17bc192f84bcc0b700cd744f37600767c6
frozenleaves <frozen@Mac.local>
2025-11-26 20:51:01 +0800

    add ut for apply_available_kernels

--- tests_v1/plugins/model_plugins/test_kernel_plugin.py
+++ tests_v1/plugins/model_plugins/test_kernel_plugin.py
@@ -42,3 +42,23 @@ class TestKernelPlugin(unittest.TestCase):
 
         model = apply_kernel(model, npu_swiglu.NpuSwiGluKernel)
         assert model.model.layers[0].mlp.forward is not original_swiglu_forward
+
+
+class Test_Use_V1_Kernels(unittest.TestCase):
+    @patch("torch.accelerator.current_accelerator")
+    def test_use_v1_kernels(self, mock_get_accelerator):
+        mock_device = MagicMock()
+        mock_device.type = "npu"
+        mock_get_accelerator.return_value = mock_device
+
+        model = AutoModelForCausalLM.from_pretrained("llamafactory/tiny-random-qwen2.5")
+
+        original_rmsnorm_forward = model.model.layers[0].input_layernorm.forward
+        original_swiglu_forward = model.model.layers[0].mlp.forward
+
+        from llamafactory.v1.plugins.model_plugins.kernels.registry import apply_available_kernels
+
+        model = apply_available_kernels(model)
+
+        assert model.model.layers[0].input_layernorm is not original_rmsnorm_forward
+        assert model.model.layers[0].mlp.forward is not original_swiglu_forward
