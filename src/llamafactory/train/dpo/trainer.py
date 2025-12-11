1fd271fb65864ba1238a383f46d1109ee58c6189
UsernameFull <tohowtodoit@gmail.com>
2025-12-11 16:45:25 +0800

    [misc] update kto/dpo fsdp/fsdp2 support

--- src/llamafactory/train/dpo/trainer.py
+++ src/llamafactory/train/dpo/trainer.py
@@ -27,6 +27,7 @@ from transformers import Trainer
 from trl import DPOTrainer
 from trl.trainer import disable_dropout_in_model
 from trl.trainer.utils import prepare_deepspeed
+from trl.models.utils import prepare_fsdp
 from typing_extensions import override
 
 from ...extras.constants import IGNORE_INDEX
@@ -97,6 +98,12 @@ class CustomDPOTrainer(DPOTrainer):
                     getattr(ref_model, "is_loaded_in_8bit", False) or getattr(ref_model, "is_loaded_in_4bit", False)
                 ):  # quantized models are already set on the correct device
                     self.ref_model = prepare_deepspeed(self.ref_model, self.accelerator)
+            elif self.is_fsdp_enabled:
+                if self.accelerator.is_fsdp2:
+                    from accelerate.utils.fsdp_utils import fsdp2_prepare_model
+                    self.ref_model = fsdp2_prepare_model(self.ref_model, self.accelerator)
+                else:
+                    self.ref_model = prepare_fsdp(self.ref_model, self.accelerator)
             else:
                 self.ref_model = self.accelerator.prepare_model(self.ref_model, evaluation_mode=True)
                 self.ref_model.eval()
