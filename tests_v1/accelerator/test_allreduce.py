4f396214877f0bfaf753e3d7b52bdace4f7cc91a
frozenleaves <frozen@Mac.local>
2025-12-15 14:45:34 +0800

    fix env set

--- tests_v1/accelerator/test_allreduce.py
+++ tests_v1/accelerator/test_allreduce.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 
 import pytest
 import torch
@@ -22,15 +21,7 @@ import torch.multiprocessing as mp
 from llamafactory.v1.accelerator.helper import ReduceOp, all_reduce, is_torch_npu_available
 
 
-os.environ["ASCEND_RT_VISIBLE_DEVICES"] = "0,1"
-
-WORLD_SIZE = 2
-
-
 def _dist_worker(rank, world_size):
-    os.environ["MASTER_ADDR"] = "127.0.0.1"
-    os.environ["MASTER_PORT"] = "29501"
-
     torch.npu.set_device(rank)
     dist.init_process_group(
         backend="hccl",
@@ -44,8 +35,6 @@ def _dist_worker(rank, world_size):
     y = torch.tensor(rank + 1.0, device="npu")
     y_sum = all_reduce(y.clone(), op=ReduceOp.SUM)
     assert y_sum.item() == 3.0
-    if rank == 0:
-        print("Case 1 Passed")
 
     # --------------------
     # Test all_reduce MEAN
@@ -66,7 +55,11 @@ def _dist_worker(rank, world_size):
     not is_torch_npu_available() or torch.npu.device_count() < 2,
     reason="Requires at least 2 NPUs",
 )
-def test_distributed_ops():
+def test_distributed_ops(monkeypatch):
+    monkeypatch.setenv("ASCEND_RT_VISIBLE_DEVICES", "0,1")
+    monkeypatch.setenv("MASTER_ADDR", "127.0.0.1")
+    monkeypatch.setenv("MASTER_PORT", "29501")
+    WORLD_SIZE = 2
     mp.spawn(
         _dist_worker,
         args=(WORLD_SIZE,),
