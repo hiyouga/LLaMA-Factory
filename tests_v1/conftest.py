be312608143a2a4f2a9a9b2d7ab20a5b00ba9133
Yaowei Zheng <hiyouga@buaa.edu.cn>
2025-12-29 19:43:55 +0800

    Update conftest.py

--- tests_v1/conftest.py
+++ tests_v1/conftest.py
@@ -143,7 +143,7 @@ def _manage_distributed_env(request: FixtureRequest, monkeypatch: MonkeyPatch) -
         monkeypatch.setenv(env_key, devices_str)
 
         # add project root dir to path for mp run
-        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
+        project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
         if project_root not in sys.path:
             sys.path.insert(0, project_root)
 
