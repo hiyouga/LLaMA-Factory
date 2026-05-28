42e5bc6a2b9ebc9561d17d37d3f2b343d7ae053d
A1waysBeenHere <moyicong1999@163.com>
2026-05-28 15:30:12 +0800

    apply liger kernel for qwen3_5_moe

--- src/llamafactory/model/model_utils/liger_kernel.py
+++ src/llamafactory/model/model_utils/liger_kernel.py
@@ -81,6 +81,8 @@ def apply_liger_kernel(
         from liger_kernel.transformers import apply_liger_kernel_to_qwen3_next as apply_liger_kernel
     elif model_type == "qwen3_5":
         from liger_kernel.transformers import apply_liger_kernel_to_qwen3_5 as apply_liger_kernel
+    elif model_type == "qwen3_5_moe":
+        from liger_kernel.transformers import apply_liger_kernel_to_qwen3_5_moe as apply_liger_kernel
     elif model_type == "gpt_oss":
         try:
             from liger_kernel.transformers import apply_liger_kernel_to_gpt_oss as apply_liger_kernel
