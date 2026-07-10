33a9700e70d7dba687fea8fdb59326ec0113c2b5
Karunanidhi Mishra <kmishra1204@hotmail.com>
2026-07-10 01:09:12 -0500

    fix: configure Qwen3.5 MoE aux loss

--- src/llamafactory/model/model_utils/moe.py
+++ src/llamafactory/model/model_utils/moe.py
@@ -176,6 +176,7 @@ def configure_moe(config: "PretrainedConfig", model_args: "ModelArguments", is_t
         "phimoe",
         "qwen2_moe",
         "qwen3_moe",
+        "qwen3_5_moe",
     ]:
         setattr(config, "output_router_logits", True)
 
@@ -195,6 +196,7 @@ def configure_moe(config: "PretrainedConfig", model_args: "ModelArguments", is_t
         "phimoe",
         "qwen2_moe",
         "qwen3_moe",
+        "qwen3_5_moe",
     ]:
         setattr(config, "router_aux_loss_coef", model_args.moe_aux_loss_coef)
 
